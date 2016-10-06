93563be9b8c1277bfe9fb68e0f4c8be5de0c2425
acethepace <akshat14009@iiitd.ac.in>
2016-10-06 23:59:46 +0530

    made basic frame

--- app/src/main/java/com/mallock/messiiitd/MainActivity.java
+++ app/src/main/java/com/mallock/messiiitd/MainActivity.java
@@ -1,13 +1,29 @@
 package com.mallock.messiiitd;
 
+import android.support.annotation.IdRes;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                if (tabId == R.id.tab_menu) {
+                    // The tab with id R.id.tab_favorites was selected,
+                    // change your content accordingly.
+                }
+            }
+        });
     }
 }
