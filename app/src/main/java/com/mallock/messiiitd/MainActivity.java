140ce7d9763dd3756d7135aa00513cb41445fc61
acethepace <akshat14009@iiitd.ac.in>
2016-10-07 00:26:37 +0530

    added lots of files

--- app/src/main/java/com/mallock/messiiitd/MainActivity.java
+++ app/src/main/java/com/mallock/messiiitd/MainActivity.java
@@ -1,9 +1,9 @@
 package com.mallock.messiiitd;
 
+import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
@@ -11,7 +11,7 @@ import com.roughike.bottombar.OnTabSelectListener;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(@Nullable final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -19,9 +19,8 @@ public class MainActivity extends AppCompatActivity {
         bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
             public void onTabSelected(@IdRes int tabId) {
-                if (tabId == R.id.tab_menu) {
-                    // The tab with id R.id.tab_favorites was selected,
-                    // change your content accordingly.
+                switch (tabId) {
+                    case R.id.tab_stats:
                 }
             }
         });
--- app/src/main/java/com/mallock/messiiitd/fragments/StatsFragment.java
+++ app/src/main/java/com/mallock/messiiitd/fragments/StatsFragment.java
@@ -1,11 +1,27 @@
 package com.mallock.messiiitd.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.mallock.messiiitd.R;
 
 /**
  * Created by Mallock on 06-10-2016.
  */
 
 public class StatsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.from(getContext())
+                .inflate(R.layout.stats_layout, container, false);
+        return view;
+    }
 
 }
