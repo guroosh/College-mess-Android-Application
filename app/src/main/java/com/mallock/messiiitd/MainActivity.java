7f58260e377ecf2c7c950f9ccd4a202ac42d7bd7
acethepace <akshat14009@iiitd.ac.in>
2016-10-09 11:11:10 +0530

    implemented fragment managing

--- app/src/main/java/com/mallock/messiiitd/MainActivity.java
+++ app/src/main/java/com/mallock/messiiitd/MainActivity.java
@@ -3,8 +3,14 @@ package com.mallock.messiiitd;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
+import com.mallock.messiiitd.fragments.StatsFragment;
+import com.mallock.messiiitd.fragments.menu.MenuFragment;
+import com.mallock.messiiitd.fragments.postWall.WallFragment;
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
 
@@ -19,9 +25,26 @@ public class MainActivity extends AppCompatActivity {
         bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
             public void onTabSelected(@IdRes int tabId) {
+                Fragment newFragment;
                 switch (tabId) {
                     case R.id.tab_stats:
+                        newFragment = new StatsFragment();
+                        break;
+                    case R.id.tab_menu:
+                        newFragment = new MenuFragment();
+                        break;
+                    case R.id.tab_wall:
+                        newFragment = new WallFragment();
+                        break;
+                    default:
+                        Toast.makeText(MainActivity.this, "UNKNOWN TAG FOR TAB", Toast.LENGTH_SHORT).show();
+                        return;
                 }
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.contentContainer, newFragment);
+//                transaction.addToBackStack(null);
+                transaction.commit();
             }
         });
     }
